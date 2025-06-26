016b69e6ef37b6f42ed76abd8a7c24ffe0c4d477
Kartik Dhawan <kartikdhawan@ideaclan.com>
2025-06-26 12:52:58 +0530

    Minor import fix

--- src/graphql/resolvers/index.ts
+++ src/graphql/resolvers/index.ts
@@ -1,6 +1,6 @@
 import { Resolvers } from "../generated/graphql.ts";
 import authMutations from "./mutations/auth.resolver.ts";
-import userMutations from "./mutations/user.resolvers.ts";
+import userMutations from "./mutations/user.resolver.ts";
 import userQueries from "./queries/user.resolver.ts";
 
 const resolvers: Resolvers = {
